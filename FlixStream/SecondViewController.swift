16573de6dd54df56424c2c4988578cfd7327d544
dariuxmx <dario@habanerostudio.mx>
2020-07-27 12:17:00 -0500

    Initial setup.

---
+++ FlixStream/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FlixStream
+//
+//  Created by Edwin Dario Gutierrez Pech on 7/27/20.
+//  Copyright © 2020 Habanero Studio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
